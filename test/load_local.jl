9166480fad29d934b14d4ea90f2f22a15642698e
milktrader <milktrader@gmail.com>
2012-12-22 13:28:47 -0500

    moved file to test/

---
+++ test/load_local.jl
@@ -0,0 +1,22 @@
+function load_local(x)
+
+# load("DataFrames")
+# load("Calendar")
+# using DataFrames
+# using Calendar
+# using UTF16
+
+time_based_df = read_table(x);
+
+# foo = convert(Array{UTF16String}, time_based_df[:,1])
+
+ bar = map(x -> parse("yyyy-MM-dd", x), convert(Array{UTF16String}, vector(time_based_df[:,1])))
+
+ within!(time_based_df, quote
+         Date = $(bar)
+         end);
+
+time_based_df
+
+end
+
