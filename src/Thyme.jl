5ce77c6dffa69095e26105f23bfbf142594d5ee6
milktrader <milktrader@gmail.com>
2012-12-22 21:08:33 -0500

    renamed load_ and building out Thyme module

---
+++ src/Thyme.jl
@@ -0,0 +1,22 @@
+require("DataFrames")
+require("Calendar")
+require("UTF16")
+
+module Thyme
+
+# import DataFrames
+# import Calendar 
+# import UTF16
+
+export load_symbol
+
+function load_symbol(x)
+  2x
+end
+
+function foo()
+  println("you can't see me")
+end
+
+
+end # of module
