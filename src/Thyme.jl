d2455a261a596550e8905b1a3157d2b4130886a6
milktrader <milktrader@gmail.com>
2012-12-24 11:43:39 -0500

    good taste macro coded

--- src/Thyme.jl
+++ src/Thyme.jl
@@ -14,23 +14,12 @@ import UTF16
 
 require("Thyme/src/read_stock.jl")
 
-export read_stock 
+export read_stock, @taste
 
-### function read_stock(x)
-### 
-### stock_df = read_table(x);
-###   time_coversion = map(x -> parse("yyyy-MM-dd", x), 
-###            convert(Array{UTF16String},  
-###                   vector(stock_df[:,1])))
-###   within!(stock_df, quote
-###          Date = $(time_coversion)
-###          end);
-###   flipud(stock_df)
-### end
-
-function foo()
-  println("you can't see me")
+macro taste(ex)
+  foo = strcat("~/.julia/Thyme/test/", :($ex), ".jl")
+  load(foo)
 end
 
 
-end # of module
+end 
