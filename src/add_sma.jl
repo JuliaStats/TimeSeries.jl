6076be01fd35bab606fb64e5755ce666be76ab1c
milktrader <milktrader@gmail.com>
2013-01-05 11:47:45 -0500

    refactored add_sma

--- src/add_sma.jl
+++ src/add_sma.jl
@@ -6,12 +6,7 @@ function add_sma(df::DataFrame, n::Int64, col::ASCIIString)
 
   function sma(x,n)
     foo = [sum(x[i:i+(n-1)])/n for i=1:length(x)-(n-1)]
-    bar = DataArray([zeros(n-1) ; foo])
-    for i in 1:length(bar)
-    if bar[i] == 0
-    bar[i] = NA
-    end
-    end
+    bar = DataArray(fill(NA, length(n) -1 ; foo])
     bar
   end
 
