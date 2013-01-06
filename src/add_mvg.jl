390b4081c9db00a6118bc49a8c1715b40af5a19e
milktrader <milktrader@gmail.com>
2013-01-06 01:02:17 -0500

    making some headway on add_mvg

---
+++ src/add_mvg.jl
@@ -0,0 +1,21 @@
+# this takes a DataFrame and returns the DataFrame with
+# an additional column whose value is defined a function
+
+
+function add_mvg(df::DataFrame, f, n::Int64, col::ASCIIString)
+
+
+function mvg(x, f, n)
+   foo = [f(x[i:i+(n-1)]) for i=1:length(x)-(n-1)]
+   bar = [nas(DataVector[float(n)], n-1) ; foo]
+  end
+
+  new_col = strcat(string(f), "_", string(n))
+
+  within!(df, quote
+    ## $new_col  = $sma(vector($df[$col]), $n)
+    $new_col  = $mvg(vector($df[$col]), $f, $n)
+        end);
+  df
+end
+
