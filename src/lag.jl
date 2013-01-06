57e8fcbeef8ae1865fa4b64daecef9f20e2fba22
milktrader <milktrader@gmail.com>
2013-01-06 16:27:08 -0500

    fixed leader laggard naming

--- src/lag.jl
+++ src/lag.jl
@@ -1,15 +1,13 @@
 
 function lead1(x)
   tmp = ones(length(x)-1)
-  laggard = [tmp[i] = x[i+1]  for i=1:length(x)-1]
-
+  leader = [tmp[i] = x[i+1]  for i=1:length(x)-1]
 # NA padding here
 end
 
 function leadn(x, n)
   tmp = ones(length(x)-n)
-  laggard = [tmp[i] = x[i+n]  for i=1:length(x)-n]
-
+  leader = [tmp[i] = x[i+n]  for i=1:length(x)-n]
 # NA padding here
 end
 
@@ -17,7 +15,6 @@ end
 function lag1(x)
   tmp = ones(length(x))
   laggard = [tmp[i] = x[i-1]  for i=2:length(x)]
-
 # NA padding here
 end
 
