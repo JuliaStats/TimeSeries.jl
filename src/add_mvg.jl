4f0455c4b4e372209b5057801561b8648ba6a9f9
milktrader <milktrader@gmail.com>
2013-01-06 08:34:56 -0500

    working add_mvg

--- src/add_mvg.jl
+++ src/add_mvg.jl
@@ -7,14 +7,13 @@ function add_mvg(df::DataFrame, col::ASCIIString, f, n::Int64)
 
   function mvg(x,f,n)
     foo = [f(x[i:i+(n-1)]) for i=1:length(x)-(n-1)]
-    bar = [nas(DataVector[float(n)], n-1) ; foo]
+    bar = [nas(DataVector[float(n)], n-1) ; float(foo)]
   end
 
   new_col = strcat(string(f), "_", string(n))
 
   within!(df, quote
-         $new_col  = $mvg(vector($df[$col]), $f, $n)
+         $new_col  = $mvg($df[$col], $f, $n)
         end);
-  df
 end
 
