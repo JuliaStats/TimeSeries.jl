3ae870aa422938f1f947d8148f508d1509672794
milktrader <milktrader@gmail.com>
2013-01-06 18:56:35 -0500

    lag1 and lagn work but lead1 is broke

--- src/lag.jl
+++ src/lag.jl
@@ -2,9 +2,14 @@
 function lead1(x)
   tmp = ones(length(x)-1)
   leader = [tmp[i] = x[i+1]  for i=1:length(x)-1]
-  padded_leader = [nas(DataVector[float(n)], 1) ; float(leader)]
+  padded_leader = [float(leader) ;  nas(DataVector[float(n)], 1)  ]
 end
 
+######## function lead1(x)
+########   tmp = ones(length(x))
+########   leader = [tmp[i] = x[i+1]  for i=1:length(x)]
+######## #  padded_leader = [nas(DataVector[float(n)], 1) ; float(leader)]
+######## end
 function leadn(x, n)
   tmp = ones(length(x)-n)
   leader = [tmp[i] = x[i+n]  for i=1:length(x)-n]
@@ -15,11 +20,12 @@ end
 function lag1(x)
   tmp = ones(length(x))
   laggard = [tmp[i] = x[i-1]  for i=2:length(x)]
-# NA padding here
+  padded_laggard = [nas(DataVector[float(n)], 1)  ; float(laggard)]
 end
 
 function lagn(x, n)
   tmp = ones(length(x))
   laggard = [tmp[i] = x[i-n]  for i=(n+1):length(x)]
+  padded_laggard = [nas(DataVector[float(n)], n)  ; float(laggard)]
 # NA padding here
 end
