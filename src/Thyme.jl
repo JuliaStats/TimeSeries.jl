de3cbc3d69a8de347e16fefcdf9c17979a639926
milktrader <milktrader@gmail.com>
2013-01-07 20:50:18 -0500

    added returns to Thyme module

--- src/Thyme.jl
+++ src/Thyme.jl
@@ -16,11 +16,13 @@ require("Thyme/src/read_stock.jl")
 require("Thyme/src/equity_curve.jl")
 require("Thyme/src/moving.jl")
 require("Thyme/src/lead_lag.jl")
+require("Thyme/src/returns.jl")
 require("Thyme/src/saute.jl")
 
 export read_stock, equity_curve,
        moving, moving!, 
        lead, lead!, lag, lag!, 
+       log_return, log_return!, simple_return, simple_return!,
        @taste, @smell 
 
 end 
