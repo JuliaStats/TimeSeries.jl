a22cde65a9848e0677c5ea4c01a650917b5a621a
milktrader <milktrader@gmail.com>
2013-01-10 13:29:31 -0500

    added colour to test suite output

--- run_tests.jl
+++ run_tests.jl
@@ -6,9 +6,10 @@ my_tests = ["test/returns.jl",
             "test/moving.jl",
             "test/read_stock.jl"]
 
-println("Running tests:")
+println("\33[36mRunning tests: \033[0m")
 
 for my_test in my_tests
-    println(" * $(my_test)")
+    println("\33[35m** \033[0m $(my_test)") 
+#    println(" * $(my_test)")
     include(my_test)
 end
