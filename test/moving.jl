4c3b68aed8af8eb9fd14b502205c0e5fa7d9c89d
milktrader <milktrader@gmail.com>
2013-01-10 13:00:40 -0500

    full test suite completed and passing

--- test/moving.jl
+++ test/moving.jl
@@ -9,7 +9,7 @@ moving!(df, "Close", min, 50);
 moving!(df, "Close", max, 50);
 
 @assert df[507,8]   == 95.8616             # 95.8616    in R's zoo::rollapply and mean
-# @assert df[507,9]   == 98.84799999999997 # 98.8480    in R's zoo::rollapply and mean
+@assert df[507,9]   == 98.84799999999997   # 98.8480    in R's zoo::rollapply and mean
 @assert df[507,10]  == 12.3920586122449    # 12.39206   in R's zoo::rollapply and var 
 @assert df[507,11]  == 0.4210029720570054  # 0.421003   in R's zoo::rollapply and PerformanceAnalytics::skewness (pandas uses biased estimator and gets a different answer)
 @assert df[507,12]  == -0.9264508782271186 # -0.9264509 in R's zoo::rollapply and PerformanceAnalytics::kurtosis (pandas uses biased estimator and gets a different answer)
