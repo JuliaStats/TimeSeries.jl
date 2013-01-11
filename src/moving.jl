0908379d1ced3bf388c18cd36f0176c463b66d3a
milktrader <milktrader@gmail.com>
2013-01-11 17:57:02 -0500

    missing colon

--- src/moving.jl
+++ src/moving.jl
@@ -1,7 +1,7 @@
 # moving returns only the padded result
 # moving! modifies the DataFrame in place
 
-function mvg(x::DataArray,f::Function,n:Int64)
+function mvg(x::DataArray,f::Function,n::Int64)
   foo = [f(x[i:i+(n-1)]) for i=1:length(x)-(n-1)]
   bar = [nas(DataVector[float(n)], n-1) ; float(foo)]
 end
