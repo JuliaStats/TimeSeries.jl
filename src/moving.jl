ae5cea9eae31cf31b64486eb9046cd81a66c3146
milktrader <milktrader@gmail.com>
2013-01-11 11:10:34 -0500

    first attempt at ema

--- src/moving.jl
+++ src/moving.jl
@@ -19,3 +19,30 @@ function moving!(df::DataFrame, col::ASCIIString, f, n::Int64)
         end);
 end
 
+########### EMA version ############################
+
+
+## first attempt  ###########
+## bad on many levels
+## the last few entries appear accurate though it takes
+## a while to stabilize
+
+function sma(x,n)
+  [mean(x[i:i+(n-1)]) for i=1:length(x)-(n-1)]
+end
+
+function ema(dv::DataArray, n:int64)
+  k = 2/(n+1)
+
+  m = sma(x, n) 
+
+  if n == 1
+    [x[i] = x[i] for i=1:length(x)]
+  else
+    x[n-1] = m[1] 
+    [x[i] = x[i]*k + [i-1]*(1-k) for i=n:length(x)]
+  end
+end
+
+
+
