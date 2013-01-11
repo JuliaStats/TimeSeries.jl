d0bad8560b5b01ca52e9ac3f35e09ab785464835
milktrader <milktrader@gmail.com>
2013-01-11 17:53:53 -0500

    added Function type to args

--- src/moving.jl
+++ src/moving.jl
@@ -1,18 +1,18 @@
 # moving returns only the padded result
 # moving! modifies the DataFrame in place
 
-function mvg(x,f,n)
+function mvg(x::DataArray,f::Function,n:Int64)
   foo = [f(x[i:i+(n-1)]) for i=1:length(x)-(n-1)]
   bar = [nas(DataVector[float(n)], n-1) ; float(foo)]
 end
 
-function moving(df::DataFrame, col::ASCIIString, f, n::Int64)
+function moving(df::DataFrame, col::ASCIIString, f::Function, n::Int64)
   with(df, quote
        $mvg($df[$col], $f, $n)
        end);
 end
 
-function moving!(df::DataFrame, col::ASCIIString, f, n::Int64)
+function moving!(df::DataFrame, col::ASCIIString, f::Function, n::Int64)
   new_col = strcat(string(f), "_", string(n))
   within!(df, quote
          $new_col  = $mvg($df[$col], $f, $n)
