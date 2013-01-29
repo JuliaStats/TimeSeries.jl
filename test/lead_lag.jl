2cb517fc01aa2bcd9c3e271ba23bb44c81e72b24
milktrader <milktrader@gmail.com>
2013-01-28 19:17:03 -0500

    tests updated for lead_lag

--- test/lead_lag.jl
+++ test/lead_lag.jl
@@ -1,27 +1,67 @@
 ######## Array ############################
 
-v      = [1,2,3,4,5]
-vlead  = lead(v)
-vlag   = lag(v)
+vi      = [1,2,3,4,5]
+vf      = [1.,2,3,4,5]
+vb      = [true, true, false, false, false]
+vs      = ["a", "b", "c", "d", "e"]
 
-@assert [2,3,4,5] == vlead 
-@assert [1,2,3,4] == vlag 
+vi_lead   = lead(vi)
+vf_lead   = lead(vf, 2)
+vb_lead   = lead(vb, 3)
+vs_lead   = lead(vs, 4)
 
-@assert 4 == length(vlead)
-@assert 4 == length(vlag)
+vi_lag   = lag(vi)
+vf_lag   = lag(vf, 2)
+vb_lag   = lag(vb, 3)
+vs_lag   = lag(vs, 4)
+
+@assert [2,3,4,5]      == vi_lead 
+@assert [3.0,4.0,5.0]  == vf_lead
+@assert [false, false] == vb_lead
+@assert ["e"]          == vs_lead
+
+@assert [1,2,3,4]       == vi_lag
+@assert [1.0, 2.0, 3.0] == vf_lag
+@assert [true, true]    == vb_lag
+@assert ["a"]           == vs_lag
+
+
+@assert 4 == length(vi_lead)
+@assert 3 == length(vf_lead)
+@assert 2 == length(vb_lead)
+@assert 1 == length(vs_lead)
+
+@assert 4 == length(vi_lag)
+@assert 3 == length(vf_lag)
+@assert 2 == length(vb_lag)
+@assert 1 == length(vs_lag)
 
 ######## DataArray ######################
 
-dv = DataArray([1,2,3,4,5]) 
+dvi = DataArray([1,2,3,4,5]) 
+dvf = DataArray([1.,2,3,4,5]) 
+dvb = DataArray([true, true, true, true, false]) 
+dvs = DataArray(["a", "b", "c", "d", "e"]) 
 
-dvlead  = lead(dv ,2)
-dvlag   = lag(dv ,2)
+dvi_lead  = lead(dv)
+dvf_lead  = lead(dv,2)
+dvb_lead  = lead(dv,3)
+dvs_lead  = lead(dv,4)
 
-#@assert [3,4,5,NA,NA] == dvlead 
-#@assert [NA,NA,1,2,3] == dvlag 
+dvi_lag = lag(dv)
+dvf_lag = lag(dv,2)
+dvb_lag = lag(dv,3)
+dvs_lag = lag(dv,4)
 
-@assert 5 == length(dvlead)
-@assert 5 == length(dvlag)
+@assert 5 == length(dvi_lead)
+@assert 5 == length(dvf_lead)
+@assert 5 == length(dvb_lead)
+@assert 5 == length(dvs_lead)
+
+@assert 5 == length(dvi_lag)
+@assert 5 == length(dvf_lag)
+@assert 5 == length(dvb_lag)
+@assert 5 == length(dvs_lag)
 
 ######## DataFrame ######################
 
@@ -34,9 +74,9 @@ lag!(df, "Close", 1)
 lag!(df, "Close", 3)
 lag!(df, "Close", 506)
 
-@assert df[1,8]    == 93.46
-@assert df[1,9]    == 92.63
-@assert df[1,10]   == 102.09
+@assert df[1,8]      == 93.46
+@assert df[1,9]      == 92.63
+@assert df[1,10]     == 102.09
 @assert df[507,11]   == 101.78
 @assert df[507,12]   == 101.95
 @assert df[507,13]   == 93.0
