8651dfa99a7cd38af0b4feceb0de14acf78a5a32
milktrader <milktrader@gmail.com>
2013-01-28 22:50:00 -0500

    moving Array works to convert to specific type but not moving DataArray

--- src/moving.jl
+++ src/moving.jl
@@ -1,11 +1,15 @@
 # moving is a simple moving window that weighs all elements equally
 
-function moving(dv::DataArray,f::Function,n::Integer)
+###       function moving{T<:Real}(dv::DataArray{T}, f::Function, n::Integer)
+###         convert(DataArray{T}, padNA([f(dv[i:i+(n-1)] for i=1:length(dv)-(n-1)]), n-1, 0))
+###       end
+
+function moving{T<:Real}(dv::DataArray{T}, f::Function, n::Integer)
   padNA(DataArray([f(dv[i:i+(n-1)]) for i=1:length(dv)-(n-1)]), n-1, 0)
 end
 
-function moving(v::Array, f::Function, n::Integer)
-  [f(v[i:i+(n-1)]) for i=1:length(v)-(n-1)]
+function moving{T<:Real}(v::Array{T}, f::Function, n::Integer)
+  convert(Array{T}, [f(v[i:i+(n-1)]) for i=1:length(v)-(n-1)])
 end
 
 function moving!(df::DataFrame, col::String, f::Function, n::Integer)
