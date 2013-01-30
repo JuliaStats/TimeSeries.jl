7ceb505fc9411374fc9001e87a5da0dfbe5144d8
milktrader <milktrader@gmail.com>
2013-01-29 21:32:09 -0500

    refactor of upto

--- src/upto.jl
+++ src/upto.jl
@@ -1,18 +1,15 @@
-
-function tothis(dv::DataArray, f::Function)
-  [f(dv[1:i]) for i=1:length(dv)]
+function upto(v::Array, f::Function)
+  [f(v[1:i]) for i=1:length(v)]
 end
 
-function upto(df::DataFrame, col::String, f::Function)
-  with(df, quote
-       $tothis($df[$col], $f)
-       end)
+function upto(dv::DataArray, f::Function)
+  [f(dv[1:i]) for i=1:length(dv)]
 end
 
 function upto!(df::DataFrame, col::String, f::Function)
   new_col = strcat(string(f), "_upto")
   within!(df, quote
-         $new_col  = $tothis($df[$col], $f)
+         $new_col  = $upto($df[$col], $f)
          end)
 end
  
