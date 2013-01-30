3a0c28934187a9d5673e9c43ea1f532e15043337
milktrader <milktrader@gmail.com>
2013-01-29 19:02:24 -0500

    moving fixed with multiline loop vs comprehension

--- src/moving.jl
+++ src/moving.jl
@@ -1,46 +1,28 @@
-######################## working #########
-
-####### Array version
+####### Array dispatch
 
 function moving{T<:Real}(v::Array{T}, f::Function, n::Integer)
   convert(Array{T}, [f(v[i:i+(n-1)]) for i=1:length(v)-(n-1)])
 end
 
-######### DataArray version
+######### DataArray dispatch  
 
-function moving(df::DataFrame, col::String, f::Function, n::Integer)
-  with(df, quote
-       $mvg($df[$col], $f, $n)
-       end)
+function moving(dv::DataArray, f::Function, n::Integer)
+  res = ones(length(dv)-(n-1))
+  for i=1:length(dv)-(n-1)
+    res[i] =  f(dv[i:i+(n-1)]) 
+  end
+  padNA(DataArray(res), n-1, 0)
 end
 
-############ DataFrames bang version 
-
-function mvg(dv::DataArray,f::Function,n::Integer)
-  foo = [f(dv[i:i+(n-1)]) for i=1:length(dv)-(n-1)]
-  bar = [nas(DataVector[float(n)], n-1) ; float(foo)]
-end
+############ DataFrames bang dispatch
 
 function moving!(df::DataFrame, col::String, f::Function, n::Integer)
   new_col = strcat(string(f), "_", string(n))
   within!(df, quote
-          $new_col  = $mvg($df[$col], $f, $n)
+          $new_col = $moving($df[$col], $f, $n)
           end)
 end
 
-################## not working refactor attempts ###########
-
-######### function moving(dv::DataArray, f::Function, n::Integer)
-#########   padNA(f([dv[i:i+(n-1)] for i=1:length(dv)-(n-1)]), n-1, 0)
-######### end
-
-### function moving!(df::DataFrame, col::String, f::Function, n::Integer)
-###   new_col = strcat(string(f), "_", string(n))
-###   within!(df, quote
-###            $new_col  = $moving($df[$col], $f, $n)
-###            end)
-### end
- 
 #################### exponential #################################
 
 function sma(x,n)
