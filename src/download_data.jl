7103e93da0b2fa754f97267d08724eae33cc252e
milktrader <milktrader@gmail.com>
2013-01-29 22:29:40 -0500

    consolidated download and parsing yahoo

--- src/download_data.jl
+++ src/download_data.jl
@@ -1,20 +1,11 @@
-function downloadPrices()
-
-  foo = readlines(`curl -s "http://ichart.finance.yahoo.com/table.csv?s=AAPL"`);
-  
+function read_yahoo(stock::String)
+  foo = readlines(`curl -s "http://ichart.finance.yahoo.com/table.csv?s=$stock"`);
   bar = foo[2:end]
-  baz  = split(bar[1], ',')'
-    
-    for i in 2:length(bar) 
-        baz  = [baz ; split(bar[i], ',')']
-    end
-  baz
-end
-
-function read_yahoo(sa)
-
+  sa  = split(bar[1], ',')'
+  for i in 2:length(bar) 
+    sa  = [sa ; split(bar[i], ',')']
+  end
   time_conversion = map(x -> parse("yyyy-MM-dd", x), convert(Array{UTF16String}, sa[:,1]))
-
   df = DataFrame(quote
      Date  = $time_conversion
      Open  = float($sa[:,2])
@@ -24,7 +15,29 @@ function read_yahoo(sa)
      Vol   =   int($sa[:,6])
      Adj   = float($sa[:,7])
      end)
-
-  flipud(df)
+  #flipud(df)
 end
+
+
+
+
+
+
+
+
+
+
+
+########### function downloadPrices(stock::String)
+########### 
+###########   foo = readlines(`curl -s "http://ichart.finance.yahoo.com/table.csv?s=$stock"`);
+###########   
+###########   bar = foo[2:end]
+###########   baz  = split(bar[1], ',')'
+###########     
+###########     for i in 2:length(bar) 
+###########         baz  = [baz ; split(bar[i], ',')']
+###########     end
+###########   baz
+########### end
 
