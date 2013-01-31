08ea578f85ab9524c272c3c01c1f4d59834f4994
milktrader <milktrader@gmail.com>
2013-01-30 23:16:12 -0500

    added period option to read_yahoo

--- src/read.jl
+++ src/read.jl
@@ -1,24 +1,33 @@
 function read_yahoo(dir::String, filename::String)
+
 csv = strcat(dir, "/", filename)
-df = read_table(csv)
+df  = read_table(csv)
+
 time_conversion = map(x -> parse("yyyy-MM-dd", x), 
                      convert(Array{UTF16String}, vector(df[:,1])))
 within!(df, quote
         Date = $(time_conversion)
         end)
+
 flipud(df)
 end
 
-function read_yahoo(stock::String, fm::Int, fd::Int, fy::Int, tm::Int, td::Int, ty::Int)
+function read_yahoo(stock::String, fm::Int, fd::Int, fy::Int, tm::Int, td::Int, ty::Int, period::String)
+
+# take care of yahoo's 0 indexing for month
   fm-=1
   tm-=1
-  ydata = readlines(`curl -s "http://ichart.finance.yahoo.com/table.csv?s=$stock&a=$fm&b=$fd&c=$fy&d=$tm&e=$td&f=$ty"`)
+
+  ydata = readlines(`curl -s "http://ichart.finance.yahoo.com/table.csv?s=$stock&a=$fm&b=$fd&c=$fy&d=$tm&e=$td&f=$ty&g=$period"`)
   numstring = ydata[2:end]
   sa  = split(numstring[1], ',')'
+
   for i in 2:length(numstring) 
     sa  = [sa ; split(numstring[i], ',')']
   end
+
   time_conversion = map(x -> parse("yyyy-MM-dd", x), convert(Array{UTF16String}, sa[:,1]))
+
   df = DataFrame(quote
      Date  = $time_conversion
      Open  = float($sa[:,2])
@@ -28,13 +37,12 @@ function read_yahoo(stock::String, fm::Int, fd::Int, fy::Int, tm::Int, td::Int, 
      Vol   =   int($sa[:,6])
      Adj   = float($sa[:,7])
      end)
+
   flipud(df)
 end
 
-read_yahoo(stock::String) = read_yahoo(stock::String,  month(now()), day(now()), year(now())-3, month(now()),  day(now()), year(now()))
-
-
-
+# default to last three years, daily data
+read_yahoo(stock::String) = read_yahoo(stock::String, month(now()), day(now()), year(now())-3, month(now()),  day(now()), year(now()), "d")
 
 # alias
 
