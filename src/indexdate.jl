3290aadb30e45738fd863c10fc0be3c9af77bcf1
Ian Fiske <ian.fiske@windlogics.com>
2013-02-15 09:22:33 -0500

    Remove unnecessary copying of data and move repetitive code into code generation.

--- src/indexdate.jl
+++ src/indexdate.jl
@@ -1,64 +1,22 @@
-function indexyear(df::DataFrame, t::Int)
-  df = copy(df)
-  df["year"] = year(df["Date"])
-  temp = df[:(year .== $t), :]
-  res  = temp[:,1:length(temp)-1]
+for (indexfun,calfun) = ((:indexyear,:year), (:indexmonth,:month), (:indexday,:day),
+                         (:indexdow,:dayofweek))
+    @eval begin
+        function ($indexfun)(df::DataFrame, t::Int)
+            userow = ($calfun)(df["Date"]) .== t
+            df[userow, :]
+        end
+    end
 end
 
-function indexmonth(df::DataFrame, t::Int)
-  df = copy(df)
-  df["month"] = month(df["Date"])
-  temp = df[:(month .== $t), :]
-  res  = temp[:,1:length(temp)-1]
-end
+####### no tests for these implementations yet
+####### After adding tests, move into the above loop
 
-function indexday(df::DataFrame, t::Int)
-  df = copy(df)
-  df["day"] = day(df["Date"])
-  temp = df[:(day .== $t), :]
-  res  = temp[:,1:length(temp)-1]
-end
-
-function indexdow(df::DataFrame, t::Int)
-  df = copy(df)
-  df["dow"] = dayofweek(df["Date"])
-  temp = df[:(dow .== $t), :]
-  res  = temp[:,1:length(temp)-1]
-end
-
-####### no tests for these implementations yet 
-
-function indexhour(df::DataFrame, t::Int)
-  df = copy(df)
-  df["hour"] = hour(df["Date"])
-  temp = df[:(hour .== $t), :]
-  res  = temp[:,1:length(temp)-1]
-end
-
-function indexminute(df::DataFrame, t::Int)
-  df = copy(df)
-  df["minute"] = minute(df["Date"])
-  temp = df[:(minute .== $t), :]
-  res  = temp[:,1:length(temp)-1]
-end
-
-function indexsecond(df::DataFrame, t::Int)
-  df = copy(df)
-  df["second"] = second(df["Date"])
-  temp = df[:(second .== $t), :]
-  res  = temp[:,1:length(temp)-1]
-end
-
-function indexweek(df::DataFrame, t::Int)
-  df = copy(df)
-  df["week"] = week(df["Date"])
-  temp = df[:(week .== $t), :]
-  res  = temp[:,1:length(temp)-1]
-end
-
-function indexdoy(df::DataFrame, t::Int)
-  df = copy(df)
-  df["doy"] = dayofyear(df["Date"])
-  temp = df[:(doy .== $t), :]
-  res  = temp[:,1:length(temp)-1]
+for (indexfun,calfun) = ((:indexhour,:hour), (:indexminute,:minute), (:indexsecond,:second),
+                         (:indexdoy,:dayofyear))
+    @eval begin
+        function ($indexfun)(df::DataFrame, t::Int)
+            userow = ($calfun)(df["Date"]) .== t
+            df[userow, :]
+        end
+    end
 end
