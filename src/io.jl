e0cba18efea01e823c0a31e7470e9ad3470dfd2a
milktrader <milktrader@gmail.com>
2013-11-02 10:56:45 -0400

    working readtime, need targeted test

--- src/io.jl
+++ src/io.jl
@@ -27,36 +27,9 @@ for col in col_that_pass
   df[col_that_pass] = Date[date(d) for d in df[col]]
 end
 
-################# # check if first row already parsed to Datetime
-################# 
-#################   typeof(df[1,1]) == Date{ISOCalendar}?
-#################   df:
-################# # otherwise find the column named case-insensitive date
-################# # this is a fragile solution that needs to instead query
-################# # the row for ISO8601 Date format and then perform the parsing
-#################   for col in colnames(df)
-#################     ismatch(r"(?i)date", col)?
-#################     df[col] = Date[date(d) for d in df[col]]:
-#################     Nothing
-#################   end
-################# 
-################# # create IndedxedVector
-#################   for col in colnames(df)
-#################     ismatch(r"(?i)date", col)?
-#################     df[col] =  IndexedVector(df[col]):
-#################     Nothing
-#################   end
-################# 
-################# # enforce descending order
-#################   for col in colnames(df)
-#################     ismatch(r"(?i)date", col) && df[1,col] > df[2, col]?
-#################     flipud!(df):
-#################     Nothing
-#################   end
-
-# create IndexedVector and  enforce descending order using first valid Datetime column
+# create IndexedVector and enforce descending order using first valid Datetime column
   enforcer     = col_that_pass[1]
-  df[enforcer] = IndexedVector(df[enforcer]):
+  df[enforcer] = IndexedVector(df[enforcer])
   df[1, enforcer] > df[2, enforcer]? flipud!(df): df
 
   return df
