886bcd3c27f2552bb7b30157c5dc3ce399a38b07
milktrader <milktrader@gmail.com>
2013-11-02 10:28:30 -0400

    started reconstructing read_time

--- src/io.jl
+++ src/io.jl
@@ -1,35 +1,58 @@
-# The regex used here is ## ismatch(r"(?i)date", col)? ##
-# and needs to be upgrded to match the column data vs column name
-
-function read_time(filename::String)
+function readtime(filename::String)
 
   df  = readtable(filename, nastrings=[".", "", "NA"])
 
-# check if first row already parsed to Datetime
-  typeof(df[1,1]) == Date{ISOCalendar}?
-  df:
-# otherwise find the column named case-insensitive date
-# this is a fragile solution that needs to instead query
-# the row for ISO8601 Date format and then perform the parsing
-  for col in colnames(df)
-    ismatch(r"(?i)date", col)?
-    df[col] = Date[date(d) for d in df[col]]:
-    Nothing
+# find columns that have been parsed as Strings by readtable
+  col_to_test = String[]
+
+  for col_data in df[1,:]
+    typeof(df[1,col_data[1]]) == UTF8String?
+      push!(col_to_test, col_data[1]):
+    nothing
   end
 
-# create IndedxedVector
-  for col in colnames(df)
-    ismatch(r"(?i)date", col)?
-    df[col] =  IndexedVector(df[col]):
-    Nothing
-  end
+# test each column's data to see if Datetime will parse it
+col_that_pass = String[]
 
-# enforce descending order
-  for col in colnames(df)
-    ismatch(r"(?i)date", col) && df[1,col] > df[2, col]?
-    flipud!(df):
-    Nothing
+for colname in col_to_test
+  d = match(r"[-|\s|\/|.]", df[1,colname])
+  d !== nothing? (bar = split(df[1, colname], d.match)): (bar = [])
+  if length(bar) == 3
+    push!(col_that_pass, colname)
   end
+end
+
+# parse column(s) that pass the Datetime regex test
+for col in col_that_pass
+  df[col_that_pass] = Date[date(d) for d in df[col]]
+end
+
+################# # check if first row already parsed to Datetime
+################# 
+#################   typeof(df[1,1]) == Date{ISOCalendar}?
+#################   df:
+################# # otherwise find the column named case-insensitive date
+################# # this is a fragile solution that needs to instead query
+################# # the row for ISO8601 Date format and then perform the parsing
+#################   for col in colnames(df)
+#################     ismatch(r"(?i)date", col)?
+#################     df[col] = Date[date(d) for d in df[col]]:
+#################     Nothing
+#################   end
+################# 
+################# # create IndedxedVector
+#################   for col in colnames(df)
+#################     ismatch(r"(?i)date", col)?
+#################     df[col] =  IndexedVector(df[col]):
+#################     Nothing
+#################   end
+################# 
+################# # enforce descending order
+#################   for col in colnames(df)
+#################     ismatch(r"(?i)date", col) && df[1,col] > df[2, col]?
+#################     flipud!(df):
+#################     Nothing
+#################   end
 
   return df
 end
