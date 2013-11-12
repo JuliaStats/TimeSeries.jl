40f0ce818240352ecf56844d18312a550f89fa03
milktrader <milktrader@gmail.com>
2013-11-11 23:26:29 -0500

    greater less equal functions appear to work, no tests

--- src/bydate.jl
+++ src/bydate.jl
@@ -1,3 +1,4 @@
+# by family
 for (byfun,calfun) = ((:byyear,:year), 
                       (:bymonth,:month), 
                       (:byday,:day),
@@ -12,3 +13,36 @@ for (byfun,calfun) = ((:byyear,:year),
         end
     end
 end
+
+# greater, less family
+
+
+for(nam, func) = ((:gtrows, :>), 
+                  (:ltrows, :<), 
+                  (:gterows, :>=), 
+                  (:lterows, :<=), 
+                  (:eqrows, :(==)))
+  @eval begin
+   function ($nam)(df::DataFrame, m::Int, d::Int, y::Int)
+      p = Int[]
+      for i in 1:nrow(df)
+        if ($func)(df[i, "Date"], date(y, m, d))
+        push!(p, i)
+        end
+      end
+    df[p,:]
+    end
+  end
+end
+
+
+# for (fun,op) = ((:gt, :.>),
+#                 (:gte, :.>=),
+#                 (:lt, :.<),
+#                 (:lte, :.<=))
+#   @eval begin
+#     function ($fun)(df::DataFrame, month::Int, day::Int, year::Int)
+#       df[:(Date ($op) $date($year, $month, $day)),:]
+#     end
+#   end
+# end
