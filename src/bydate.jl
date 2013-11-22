59355aa14f8db99b37e1171fbc39a718231a1657
milktrader <milktrader@gmail.com>
2013-11-21 21:53:24 -0500

    rm OHLC and starting to remove gte, etc

--- src/bydate.jl
+++ src/bydate.jl
@@ -14,35 +14,3 @@ for (byfun,calfun) = ((:byyear,:year),
     end
 end
 
-# greater, less family
-
-
-for(nam, func) = ((:gtrows, :>), 
-                  (:ltrows, :<), 
-                  (:gterows, :>=), 
-                  (:lterows, :<=), 
-                  (:eqrows, :(==)))
-  @eval begin
-   function ($nam)(df::DataFrame, m::Int, d::Int, y::Int)
-      p = Int[]
-      for i in 1:nrow(df)
-        if ($func)(df[i, "Date"], date(y, m, d))
-        push!(p, i)
-        end
-      end
-    df[p,:]
-    end
-  end
-end
-
-
-# for (fun,op) = ((:gt, :.>),
-#                 (:gte, :.>=),
-#                 (:lt, :.<),
-#                 (:lte, :.<=))
-#   @eval begin
-#     function ($fun)(df::DataFrame, month::Int, day::Int, year::Int)
-#       df[:(Date ($op) $date($year, $month, $day)),:]
-#     end
-#   end
-# end
