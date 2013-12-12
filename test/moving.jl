4848602ea8c590b9cf89b4732499e884f65a1f0f
milktrader <milktrader@gmail.com>
2013-12-11 22:51:14 -0500

    using test_approx_eq to eliminate rounding errors

--- test/moving.jl
+++ test/moving.jl
@@ -62,13 +62,14 @@ let
   moving!(df, "Close", minimum, 50)
   moving!(df, "Close", maximum, 50)
   
-  @assert df[507,8]   == 95.8616             # 95.8616    in R's zoo::rollapply and mean
-  @assert df[507,9]   == 98.84799999999997   # 98.8480    in R's zoo::rollapply and mean
-  @assert df[507,10]  == 12.3920586122449    # 12.39206   in R's zoo::rollapply and var 
+  @assert df[507,8]   == 95.8616              # 95.8616    in R's zoo::rollapply and mean
+  #@assert df[507,9]   == 98.84799999999997   # 98.8480    in R's zoo::rollapply and mean
+  @test_approx_eq df[507,9]   == 98.8480      # 98.8480    in R's zoo::rollapply and mean
+  @assert df[507,10]  == 12.3920586122449     # 12.39206   in R's zoo::rollapply and var 
   @assert df[507,11]  == 0.42100297205700554  # 0.421003   in R's zoo::rollapply and PerformanceAnalytics::skewness (pandas uses biased estimator and gets a different answer)
-  @assert df[507,12]  == -0.9264508782271186 # -0.9264509 in R's zoo::rollapply and PerformanceAnalytics::kurtosis (pandas uses biased estimator and gets a different answer)
-  @assert df[507,13]  == 90.16               # 90.16      in R's zoo::rollapply and min 
-  @assert df[507,14]  == 102.21              # 102.21     in R's zoo::rollapply and max 
+  @assert df[507,12]  == -0.9264508782271186  # -0.9264509 in R's zoo::rollapply and PerformanceAnalytics::kurtosis (pandas uses biased estimator and gets a different answer)
+  @assert df[507,13]  == 90.16                # 90.16      in R's zoo::rollapply and min 
+  @assert df[507,14]  == 102.21               # 102.21     in R's zoo::rollapply and max 
                                       
   # dv1 = df["Open"];
   # dv2 = df["Close"];
