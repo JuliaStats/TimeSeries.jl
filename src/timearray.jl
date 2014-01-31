2df27bf1ec4fc344d6b96b86673203278ae70290
milktrader <milktrader@gmail.com>
2014-01-31 18:08:22 -0500

    started outer constructor for TimeArray

--- src/timearray.jl
+++ src/timearray.jl
@@ -1,10 +1,53 @@
 type TimeArray{T,N}
   timestamp::Array{Date{ISOCalendar},1}
   values::Array{T,N}
-  colnames::Array(String,1}
+  colnames::Array{String,1}
+end
+ 
+function TimeArray{T}(args::Array{SeriesPair{Date{ISOCalendar}, T},1}...)
+  
+  # create array of index values from args
+  allkey = Date{ISOCalendar}[]
+  for arg in args
+    for ar in arg
+      push!(allkey, ar.index)
+    end
+  end
+
+  # and sort without duplicates
+  key = sortandremoveduplicates(allkey)
+
+  # match each arg in args with key 
+  arr = fill(NaN, length(key), length(args))
+    for i in 1:length(args)
+      for j = 1:length(args[i])
+        t = args[i][j].index .== key
+        k = findfirst(t)
+        arr[k,i] = args[i][j].value 
+      end
+    end
+  
+  # finally get an array of the names
+  nams = String[arg[1].name for arg in args]
+
+  TimeArray(key, arr, nams)
 end
 
- 
+#################################
+# sortandremoveduplicates #######
+#################################
+
+function sortandremoveduplicates(x::Array)
+  sx = sort(x)
+  res = [sx[1]]
+  for i = 2:length(sx)
+    if sx[i] > sx[i-1]
+    push!(res, sx[i])
+    end
+  end
+  res
+end
+
 #################################
 ###### length ###################
 #################################
@@ -18,6 +61,7 @@ end
 #################################
  
 function Base.show(io::IO, ta::TimeArray)
+  println("summary of dimensions, etc")
   for p in 1:length(ta.colnames)
     print(io, "   ", ta.colnames[p])
   end
@@ -36,10 +80,10 @@ end
 #################################
 
 #getindex{T,N}(ta::TimeArray{T,N}, n::Int64)
-getindex(ta::TimeArray, n::Int64)
-  [ta.timestamp[n] ta.values[n]] 
-end
-
+# getindex(ta::TimeArray, n::Int64)
+#   [ta.timestamp[n] ta.values[n]] 
+# end
+# 
 
 ## function getindex{T <: Date{ISOCalendar}, V}(sa::Array{SeriesPair{T, V}, 1}, mydate::Date{ISOCalendar})
 ##   for i in 1:size(sa,1)
