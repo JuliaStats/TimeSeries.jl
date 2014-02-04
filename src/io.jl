ce01ead9363e30e246bcfa5f448c3faacd3e452f
milktrader <milktrader@gmail.com>
2014-02-04 16:51:31 -0500

    merged immutable, no major breakage

---
+++ src/io.jl
@@ -0,0 +1,10 @@
+function readtimearray(fname::String)
+  blob    = readcsv(fname)
+  tstamps = Date{ISOCalendar}[date(i) for i in blob[2:end, 1]]
+  vals    = float(blob[2:end, 2:end])
+  cnames  = ASCIIString[]
+  for b in blob[1, 2:end]
+    push!(cnames, b)
+  end
+  TimeArray(tstamps, vals, cnames)
+end
