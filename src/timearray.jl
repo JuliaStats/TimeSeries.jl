70cf13cc394a8c6b3d768d10a0ebc895a0cedcc8
milktrader <milktrader@gmail.com>
2014-02-05 23:31:34 -0500

    removed operators files and put code generation code in timearray.jl along with the type definition

--- src/timearray.jl
+++ src/timearray.jl
@@ -146,3 +146,39 @@ end
 # Base.getindex{T,N}(ta::TimeArray{T,N}, d::DAYOFWEEK) = ta[dayofweek(ta.timestamp) .== d]
 
 
+#################################
+###### +, -, *, / ###############
+#################################
+ 
+# this function generating code needs to live in the same file as the type definition
+# or in the module file for some unexplained scoping issue
+
+# element-wise operations between two columns
+#for op in [:.+, :.-, :.*, :./, :.>, :.<, :.>=, :.<=, :.==] # return Bools for comparison operators?
+for op in [:.+, :.-, :.*, :./]
+  @eval begin
+    function ($op){T}(ta1::TimeArray{T,1}, ta2::TimeArray{T,1})
+      cname  = [ta1.colnames[1][1:2] *  string($op) *  ta2.colnames[1][1:2]]
+      tstamp = Date{ISOCalendar}[]
+      vals   = T[]
+      for i in 1:size(ta1.timestamp, 1)
+        for j in 1:size(ta2.timestamp, 1)
+          if ta1.timestamp[i] == ta2.timestamp[j] 
+            push!(tstamp, ta1.timestamp[i]) 
+            push!(vals, ($op)(ta1.values[i], ta2.values[j])) 
+          end
+        end
+      end
+      TimeArray(tstamp, vals, cname)
+    end # function
+  end # eval
+end # loop
+
+# # operations between a column and Int,Float64
+# for op in [:+, :-, :*, :/, :.+, :.-, :.*, :./]
+#   @eval begin
+#     function ($op){T,V}(sp::SeriesPair{T,V}, var::Union(Int,Float64))
+#       SeriesPair(sp.timestamp, ($op)(sp.value, var))
+#     end
+#   end
+# end
