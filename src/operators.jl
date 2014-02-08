1693bf2401e110c9f00bca6fb883622dab7123c0
milktrader <milktrader@gmail.com>
2014-02-08 11:42:04 -0500

    comparison operators work on two columns and colum with int/float, but no tests yest

--- src/operators.jl
+++ src/operators.jl
@@ -23,24 +23,24 @@ for op in [:.+, :.-, :.*, :./]
 end # loop
 
 # element-wise comparison operations between two columns
-# for op in [:.>, :.<, :.==, :.>=, :<=]
-#   @eval begin
-#     function ($op){T,N}(ta1::TimeArray{T,N}, ta2::TimeArray{T,N})
-#       cname  = [ta1.colnames[1][1:2] *  string($op) *  ta2.colnames[1][1:2]]
-#       tstamp = Date{ISOCalendar}[]
-#       vals   = T[]
-#       for i in 1:size(ta1.timestamp, 1)
-#         for j in 1:size(ta2.timestamp, 1)
-#           if ta1.timestamp[i] == ta2.timestamp[j] 
-#             push!(tstamp, ta1.timestamp[i]) 
-#             push!(vals, ($op)(ta1.values[i], ta2.values[j])) 
-#           end
-#         end
-#       end
-#       TimeArray(tstamp, vals, cname)
-#     end # function
-#   end # eval
-# end # loop
+for op in [:.>, :.<, :.==, :.>=, :<=]
+  @eval begin
+    function ($op){T,N}(ta1::TimeArray{T,N}, ta2::TimeArray{T,N})
+      cname  = [ta1.colnames[1][1:2] *  string($op) *  ta2.colnames[1][1:2]]
+      tstamp = Date{ISOCalendar}[]
+      vals   = Bool[]
+      for i in 1:size(ta1.timestamp, 1)
+        for j in 1:size(ta2.timestamp, 1)
+          if ta1.timestamp[i] == ta2.timestamp[j] 
+            push!(tstamp, ta1.timestamp[i]) 
+            push!(vals, ($op)(ta1.values[i], ta2.values[j])) 
+          end
+        end
+      end
+      TimeArray(tstamp, vals, cname)
+    end # function
+  end # eval
+end # loop
 
 # element-wise mathematical operations between a column and Int,Float64
 for op in [:.+, :.-, :.*, :./]
