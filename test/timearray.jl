ad36dbbed8ce5ffbb305379da76bb0c6ccfdd016
milktrader <milktrader@gmail.com>
2014-06-30 09:40:55 -0400

    add metadata field to TimeArray type closes #126

--- test/timearray.jl
+++ test/timearray.jl
@@ -2,26 +2,36 @@ using MarketData
 
 facts("type constructors enforce invariants") do
 
-  context("unequal length between values and timestamp fails") do
-      @fact_throws TimeArray(cl.timestamp, cl.values[2:end], ["Close"])
-  end
-
-  context("unequal length between colnames and array width fails") do
-    @fact_throws TimeArray(cl.timestamp, cl.values, ["Close", "Open"])
-  end
-
-  context("duplicate timestamp values fails") do
-    @fact_throws TimeArray(dupestamp, push!(cl.values, cl.values[1]), ["Close"])
-  end
-
-  context("mangled order of timestamp values fails") do
-    @fact_throws TimeArray(mangstamp,  push!(cl.values, cl.values[1]), ["Close"])
-  end
-
-  context("flipping occurs when needed") do
-    @fact TimeArray(flipud(cl.timestamp), flipud(cl.values),  ["Close"]).timestamp[1] => Date(2000,1,3)
-    @fact TimeArray(flipud(cl.timestamp), flipud(cl.values),  ["Close"]).values[1]    => 111.94
-  end
+    context("unequal length between values and timestamp fails") do
+        @fact_throws TimeArray(cl.timestamp, cl.values[2:end], ["Close"])
+    end
+  
+    context("unequal length between colnames and array width fails") do
+        @fact_throws TimeArray(cl.timestamp, cl.values, ["Close", "Open"])
+    end
+  
+    context("duplicate timestamp values fails") do
+        @fact_throws TimeArray(dupestamp, push!(cl.values, cl.values[1]), ["Close"])
+    end
+  
+    context("mangled order of timestamp values fails") do
+        @fact_throws TimeArray(mangstamp,  push!(cl.values, cl.values[1]), ["Close"])
+    end
+  
+    context("flipping occurs when needed") do
+        @fact TimeArray(flipud(cl.timestamp), flipud(cl.values),  ["Close"]).timestamp[1] => Date(2000,1,3)
+        @fact TimeArray(flipud(cl.timestamp), flipud(cl.values),  ["Close"]).values[1]    => 111.94
+    end
+  
+    context("metadata defaults to Nothing") do
+        @fact cl.metadata => Nothing
+    end
+  
+    context("metadata field can be assigned") do
+        clm = TimeArray(cl.timestamp, cl.values, cl.colnames, 2)
+        @fact typeof(clm.metadata) => Int
+        @fact clm.metadata         => 2
+    end
 end
   
 facts("conversion methods") do
@@ -33,34 +43,34 @@ end
 
 facts("getindex methods") do
   
-  context("getindex on single Int and DateTime") do
-    @fact ohlc[1].timestamp        => [Date(2000,1,3)]
-    @fact ohlc[Date(2000,1,3)].timestamp => [Date(2000,1,3)]
-  end
-  
-  context("getindex on array of Int and DateTime") do
-    @fact ohlc[[1,10]].timestamp              => [Date(2000,1,3), Date(2000,1,14)]
-    @fact ohlc[[Date(2000,1,3),Date(2000,1,14)]].timestamp => [Date(2000,1,3), Date(2000,1,14)]
-  end
+    context("getindex on single Int and DateTime") do
+        @fact ohlc[1].timestamp        => [Date(2000,1,3)]
+        @fact ohlc[Date(2000,1,3)].timestamp => [Date(2000,1,3)]
+    end
+    
+    context("getindex on array of Int and DateTime") do
+        @fact ohlc[[1,10]].timestamp              => [Date(2000,1,3), Date(2000,1,14)]
+        @fact ohlc[[Date(2000,1,3),Date(2000,1,14)]].timestamp => [Date(2000,1,3), Date(2000,1,14)]
+    end
 
-  context("getindex on range of Int and DateTime") do
-    @fact ohlc[1:2].timestamp                        => [Date(2000,1,3), Date(2000,1,4)]
-    @fact ohlc[Date(2000,1,3):Day(1):Date(2000,1,4)].timestamp => [Date(2000,1,3), Date(2000,1,4)]
-  end
+    context("getindex on range of Int and DateTime") do
+        @fact ohlc[1:2].timestamp                        => [Date(2000,1,3), Date(2000,1,4)]
+        @fact ohlc[Date(2000,1,3):Day(1):Date(2000,1,4)].timestamp => [Date(2000,1,3), Date(2000,1,4)]
+    end
 
-  context("getindex on single column name") do
-    @fact size(ohlc["Open"].values, 2)                            => 1
-    @fact size(ohlc["Open"][Date(2000,1,3):Day(1):Date(2000,1,14)].values, 1) => 10
-  end
+    context("getindex on single column name") do
+        @fact size(ohlc["Open"].values, 2)                            => 1
+        @fact size(ohlc["Open"][Date(2000,1,3):Day(1):Date(2000,1,14)].values, 1) => 10
+    end
 
-  context("getindex on multiple column name") do
-    @fact ohlc["Open", "Close"].values[1]   => 104.88
-    @fact ohlc["Open", "Close"].values[2]   => 108.25
-    @fact ohlc["Open", "Close"].values[501] => 111.94
-  end
+    context("getindex on multiple column name") do
+        @fact ohlc["Open", "Close"].values[1]   => 104.88
+        @fact ohlc["Open", "Close"].values[2]   => 108.25
+        @fact ohlc["Open", "Close"].values[501] => 111.94
+    end
 
-  context("getindex on 1d returns 1d object") do
-    @fact isa(cl[1], TimeArray{Float64,1})   => true
-    @fact isa(cl[1:2], TimeArray{Float64,1}) => true
-  end
+    context("getindex on 1d returns 1d object") do
+        @fact isa(cl[1], TimeArray{Float64,1})   => true
+        @fact isa(cl[1:2], TimeArray{Float64,1}) => true
+    end
 end
