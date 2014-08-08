9acf25cb8a09382b3b829b8c6d475136ded577f0
milktrader <milktrader@gmail.com>
2014-08-08 09:53:17 -0400

    updated tests to cover readtimearray parsing seconds data

--- test/readwrite.jl
+++ test/readwrite.jl
@@ -2,16 +2,17 @@ using MarketData
 
 facts("readwrite parses csv file correctly") do
 
-  context("1-d values array works") do
+  context("1d values array works") do
       @fact typeof(cl.values) => Array{Float64,1}
   end
 
-  context("1-d values array works") do
+  context("2d values array works") do
       @fact typeof(ohlc.values) => Array{Float64,2}
   end
 
   context("timestamp parses to correct type") do
-      @fact typeof(cl.timestamp) => Array{Date,1}
+      @fact typeof(cl.timestamp)    => Vector{Date}
+      @fact typeof(sdata.timestamp) => Vector{DateTime}
   end
 end
 
