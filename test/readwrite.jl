4ebb4f867bde4cf1869900320d1b7751c17bc1ca
Gord Stephen <gord@gordstephen.ca>
2015-09-04 10:22:05 -0400

    Replaced FactCheck-depricated => with -->

--- test/readwrite.jl
+++ test/readwrite.jl
@@ -3,27 +3,27 @@ using MarketData
 facts("readwrite parses csv file correctly") do
 
   context("1d values array works") do
-      @fact typeof(cl.values) => Array{Float64,1}
+      @fact typeof(cl.values) --> Array{Float64,1}
   end
 
   context("2d values array works") do
-      @fact typeof(ohlc.values) => Array{Float64,2}
+      @fact typeof(ohlc.values) --> Array{Float64,2}
   end
 
   context("Specifying DateTime string format for reading") do
       ta = readtimearray(Pkg.dir("TimeSeries/test/data/datetime3.csv"), format="yyyy/mm/dd|HH:MM:SS")
-      @fact length(ta) => 5
-      @fact size(ta.values) => (5,1)
-      @fact ta.timestamp[4] => DateTime(2010,1,4,9,4)
+      @fact length(ta) --> 5
+      @fact size(ta.values) --> (5,1)
+      @fact ta.timestamp[4] --> DateTime(2010,1,4,9,4)
       @fact_throws readtimearray(Pkg.dir("TimeSeries/test/data/datetime3.csv"))
   end
 
   context("timestamp parses to correct type") do
-      @fact typeof(cl.timestamp)        => Vector{Date}
-      @fact typeof(datetime1.timestamp) => Vector{DateTime}
+      @fact typeof(cl.timestamp)        --> Vector{Date}
+      @fact typeof(datetime1.timestamp) --> Vector{DateTime}
   end
 
   context("readtimearray accepts meta field") do
-      @pending mdata => "construct mdata from csv vs reconstructing a Time Array" 
+      @pending mdata --> "construct mdata from csv vs reconstructing a Time Array" 
   end
 end
--- test/split.jl
+++ test/split.jl
@@ -3,38 +3,38 @@ using MarketData
 facts("find methods") do
 
   context("findall returns correct row numbers array") do
-     @fact cl[findall(cl .> op)].timestamp[1] => Date(2000,1,3)
-     @fact length(findall(cl .> op))          => 244
+     @fact cl[findall(cl .> op)].timestamp[1] --> Date(2000,1,3)
+     @fact length(findall(cl .> op))          --> 244
   end
 
   context("findwhen returns correct Dates array") do
-#     @fact findwhen(cl .> op)[1]      => Date(2000,1,3)
-#     @fact length(findwhen(cl .> op)) => 244 
+#     @fact findwhen(cl .> op)[1]      --> Date(2000,1,3)
+#     @fact length(findwhen(cl .> op)) --> 244 
   end
 end
 
 facts("split date operations") do
 
   context("from and to correctly subset") do
-     @fact length(from(cl, 2001,12,28)) => 2
-     @fact length(to(cl, 2000,1,4))     => 2
+     @fact length(from(cl, 2001,12,28)) --> 2
+     @fact length(to(cl, 2000,1,4))     --> 2
   end
 
   context("bydate methods correctly subset") do
-     @fact by(cl,2001, period=year).timestamp[1]   => Date(2001,1,2)
-     @fact by(cl,2, period=month).timestamp[1]     => Date(2000,2,1)
-     @fact by(cl,4, period=day).timestamp[1]       => Date(2000,1,4) 
-     @fact by(cl,5, period=dayofweek).timestamp[1] => Date(2000,1,7)
-     @fact by(cl,4, period=dayofyear).timestamp[1] => Date(2000,1,4)
+     @fact by(cl,2001, period=year).timestamp[1]   --> Date(2001,1,2)
+     @fact by(cl,2, period=month).timestamp[1]     --> Date(2000,2,1)
+     @fact by(cl,4, period=day).timestamp[1]       --> Date(2000,1,4) 
+     @fact by(cl,5, period=dayofweek).timestamp[1] --> Date(2000,1,7)
+     @fact by(cl,4, period=dayofyear).timestamp[1] --> Date(2000,1,4)
   end
 end
 
 facts("element wrappers") do
 
   context("type element wrappers isolate elements") do
-     @fact isa(timestamp(cl), Array{Date,1}) => true
-     @fact isa(values(cl), Array{Float64,1})              => true
-     @fact isa(values(ohlc), Array{Float64,2})            => true
-     @fact isa(colnames(cl), Array{UTF8String, 1})       => true
+     @fact isa(timestamp(cl), Array{Date,1}) --> true
+     @fact isa(values(cl), Array{Float64,1})              --> true
+     @fact isa(values(ohlc), Array{Float64,2})            --> true
+     @fact isa(colnames(cl), Array{UTF8String, 1})       --> true
   end
 end
