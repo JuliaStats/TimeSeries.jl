d732cc64a9065e216207eb59366860b66c4ed2e1
Michael K. Borregaard <mkborregaard@snm.ku.dk>
2017-02-09 20:01:25 +0100

    4 space indents

--- src/plotrecipes.jl
+++ src/plotrecipes.jl
@@ -1,6 +1,6 @@
 
 @recipe function f{T<:TimeArray}(ta::T)
-       labels --> reshape(ta.colnames,1,length(ta.colnames))
-       seriestype --> :path
-       ta.timestamp, ta.values
+    labels --> reshape(ta.colnames,1,length(ta.colnames))
+    seriestype --> :path
+    ta.timestamp, ta.values
 end
