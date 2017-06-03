93feab54dfc1a695c2d195a04bd77b1971d8c75a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-06-02 20:06:54 -0400

    Update timearray.jl

--- test/timearray.jl
+++ test/timearray.jl
@@ -56,16 +56,18 @@ facts("type constructors enforce invariants") do
 end
 
 facts("type constructors allow views") do
-    
+
     source_rows = 101:121
-    source_cols = 1:size(AAPL.values)[2]    
-    
-    AAPL1 = TimeArray(AAPL.timestamp[source_rows], AAPL.values[source_rows, source_cols], AAPL.colnames, AAPL.meta)
-    
+    source_cols = 1:size(AAPL.values)[2]
+
+    AAPL1 = TimeArray(AAPL.timestamp[source_rows],
+                      AAPL.values[source_rows, source_cols],
+                      AAPL.colnames, AAPL.meta)
+
     tstamps = view(AAPL.timestamp, source_rows)
     tvalues = view(AAPL.values, source_rows, source_cols)
-    
-    APPL2 = TimeArray(tstamps, tvalues, AAPL.colnames, AAPL.meta)
+
+    AAPL2 = TimeArray(tstamps, tvalues, AAPL.colnames, AAPL.meta)
 
     @fact length(AAPL1) --> length(AAPL2)
     @fact AAPL1[5].values[3] --> AAPL2[5].values[3]
@@ -76,8 +78,8 @@ facts("type constructors allow views") do
     context("match final values") do
         @fact AAPL[121].values --> AAPL2[end].values)
     end
-=#   
-end    
+=#
+end
 
 facts("construction without colnames") do
 
