3f1147d5ca1346fee186a95424e67d6ee4f97e0d
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-06-27 22:50:23 +0800

    test: migrate modify.jl

--- test/modify.jl
+++ test/modify.jl
@@ -1,9 +1,15 @@
-using TimeSeries, MarketData, Base.Dates
-FactCheck.setstyle(:compact)
-FactCheck.onlystats(true)
+using Base.Dates
+using Base.Test
 
-facts("update method works") do
+using MarketData
 
+using TimeSeries
+
+
+@testset "modify" begin
+
+
+@testset "update method works" begin
     new_cls  = update(cl, today(), 111.11)
     new_clv  = update(cl, today(), [111.11])
     new_ohlc = update(ohlc, today(), [111.11 222.22 333.33 444.44])
@@ -12,70 +18,75 @@ facts("update method works") do
     empty1   = TimeArray(Vector{Date}(), Array{Int}(0,1))
     empty2   = TimeArray(Vector{Date}(), Array{Int}(0,2))
 
-    context("empty time arrays can be constructed") do
-        @fact length(empty1) --> 0
-        @fact length(empty2) --> 0
+    @testset "empty time arrays can be constructed" begin
+        @test length(empty1) == 0
+        @test length(empty2) == 0
     end
 
-    context("update an empty time array fails") do
-        @fact_throws update(empty1, Date(2000,1,1), 10)
-        @fact_throws update(empty2, Date(2000,1,1), [10 11])
-#         @fact length(update1)      --> 1
-#         @fact length(update2)      --> 1
-#         @fact update1.timestamp[1] --> Date(2000,1,1)
-#         @fact update2.timestamp[1] --> Date(2000,1,1)
-#         @fact update1.values       --> [12]
-#         @fact update2.values       --> [10 11]
-#         @fact update1.colnames     --> ["foo"]
-#         @fact update2.colnames     --> ["foo", "bar"]
-#         @fact update1.meta         --> "bar"
-#         @fact update2.meta         --> "baz"
+    @testset "update an empty time array fails" begin
+        @test_throws ErrorException update(empty1, Date(2000,1,1), 10)
+        @test_throws ErrorException update(empty2, Date(2000,1,1), [10 11])
+        # @test length(update1)      == 1
+        # @test length(update2)      == 1
+        # @test update1.timestamp[1] == Date(2000,1,1)
+        # @test update2.timestamp[1] == Date(2000,1,1)
+        # @test update1.values       == [12]
+        # @test update2.values       == [10 11]
+        # @test update1.colnames     == ["foo"]
+        # @test update2.colnames     == ["foo", "bar"]
+        # @test update1.meta         == "bar"
+        # @test update2.meta         == "baz"
     end
 
-    context("update a single column time array with single value vector") do
-        @fact last(new_clv.values) --> 111.11
-        @fact_throws update(cl, today(), [111.11, 222.22])
+    @testset "update a single column time array with single value vector" begin
+        @test last(new_clv.values) == 111.11
+        @test_throws ErrorException update(cl, today(), [111.11, 222.22])
     end
 
-    context("update a multi column time array") do
-        #@fact last(new_ohlc).values --> [111.11 222.22 333.33 444.44]
-        @fact tail(new_ohlc).values --> [111.11 222.22 333.33 444.44]
-        @fact_throws update(ohlc, today(),  [111.11, 222.22, 333.33])
+    @testset "update a multi column time array" begin
+        # @test last(new_ohlc).values == [111.11 222.22 333.33 444.44]
+        @test tail(new_ohlc).values == [111.11 222.22 333.33 444.44]
+        @test_throws MethodError update(ohlc, today(), [111.11, 222.22, 333.33])
     end
 
-    context("cannot update more than one observation at a time") do
-        @fact_throws update(cl, [Date(2002,1,1), Date(2002,1,2)], [111.11, 222,22])
+    @testset "cannot update more than one observation at a time" begin
+        @test_throws(
+            MethodError,
+            update(cl, [Date(2002,1,1), Date(2002,1,2)], [111.11, 222,22]))
     end
 
-    context("cannot update oldest observations") do
-        @fact_throws update(cl, Date(1999,1,1), [111.11])
-        @fact_throws update(cl, Date(1999,1,1), 111.11)
+    @testset "cannot update oldest observations" begin
+        @test_throws ErrorException update(cl, Date(1999,1,1), [111.11])
+        @test_throws ErrorException update(cl, Date(1999,1,1), 111.11)
     end
 
-    context("cannot update in-between observations") do
-        @fact_throws update(cl, Date(2000,1,8), [111.11])
-        @fact_throws update(cl, Date(2000,1,8), 111.11)
+    @testset "cannot update in-between observations" begin
+        @test_throws ErrorException update(cl, Date(2000,1,8), [111.11])
+        @test_throws ErrorException update(cl, Date(2000,1,8), 111.11)
+    end
+end
+
+
+@testset "rename method works" begin
+    re_ohlc = rename(ohlc, ["a","b","c","d"])
+    re_cl   = rename(cl, ["vector"])
+    re_cls  = rename(cl, "string")
+
+    @testset "change colnames with multi-member vector" begin
+        @test colnames(re_ohlc) == ["a","b","c","d"]
+        @test_throws ErrorException rename(ohlc, ["a"])
+    end
+
+    @testset "change colnames with single-member vector" begin
+        @test colnames(re_cl) == ["vector"]
+        @test_throws ErrorException rename(cl, ["a", "b"])
+    end
+
+    @testset "change colnames with string" begin
+        @test colnames(re_cls) == ["string"]
+        @test_throws MethodError rename(cl, "string_a", "string_b")
     end
 end
 
-facts("rename method works") do
 
-    re_ohlc = rename(ohlc, ["a","b","c","d"]) 
-    re_cl   = rename(cl, ["vector"]) 
-    re_cls  = rename(cl, "string") 
-
-    context("change colnames with multi-member vector") do
-        @fact colnames(re_ohlc) --> ["a","b","c","d"]
-        @fact_throws rename(ohlc, ["a"])
-    end
-
-    context("change colnames with single-member vector") do
-        @fact colnames(re_cl) --> ["vector"]
-        @fact_throws rename(cl, ["a", "b"])
-    end
-
-    context("change colnames with string") do
-        @fact colnames(re_cls) --> ["string"]
-        @fact_throws rename(cl, "string_a", "string_b")
-    end
-end
+end  # @testset "modify"
