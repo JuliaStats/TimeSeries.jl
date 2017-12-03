5fa806472ebe7957a87707228b0ab305da1afd60
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-12-03 20:15:07 +0800

    timearray: more Base function support (#338)
    - sum
    - mean
    - std
    - var
    - cumprod

--- test/basemisc.jl
+++ test/basemisc.jl
@@ -8,7 +8,7 @@ using TimeSeries
 @testset "basemisc" begin
 
 
-@testset "cumsum" begin
+@testset "cumulative functions" begin
     let ta = cumsum(cl)
         @test ta.values == cumsum(cl.values)
         @test ta.meta == cl.meta
@@ -28,6 +28,76 @@ using TimeSeries
         @test ta.values == cumsum(ohlc.values, 2)
         @test ta.meta == ohlc.meta
     end
+
+    @test_throws DimensionMismatch cumsum(cl, 3)
+    @test_throws DimensionMismatch cumsum(ohlc, 3)
+
+    let ta = cumprod(cl[1:5])
+        @test ta.values == cumprod(cl[1:5].values)
+        @test ta.meta == cl[1:5].meta
+    end
+
+    let ta = cumprod(ohlc[1:5])
+        @test ta.values == cumprod(ohlc[1:5].values)
+        @test ta.meta == ohlc[1:5].meta
+    end
+
+    let ta = cumprod(cl[1:5], 2)
+        @test ta.values == cumprod(cl[1:5].values, 2)
+        @test ta.meta == cl[1:5].meta
+    end
+
+    let ta = cumprod(ohlc[1:5], 2)
+        @test ta.values == cumprod(ohlc[1:5].values, 2)
+        @test ta.meta == ohlc[1:5].meta
+    end
+
+    @test_throws DimensionMismatch cumprod(cl, 3)
+    @test_throws DimensionMismatch cumprod(ohlc, 3)
+end
+
+@testset "reduction functions" begin
+    for (fname, f) ∈ ([(:sum, sum), (:mean, mean)])
+        for (name, src) ∈ [(:cl, cl), (:ohlc, ohlc)]
+            @testset "$fname::$name" begin
+                let ta = f(src)
+                    @test ta.meta == src.meta
+                    @test length(ta) == 1
+                    @test ta.values == f(src.values, 1)
+                end
+
+                let ta = f(src, 2)
+                    @test ta.meta == src.meta
+                    @test length(ta) == length(src.timestamp)
+                    @test ta.values == f(src.values, 2)
+                    @test ta.colnames == [string(fname)]
+                end
+
+                @test_throws DimensionMismatch f(src, 3)
+            end  # @testset
+        end
+    end  # for func
+
+    for (fname, f) ∈ ([(:std, std), (:var, var)])
+        for (name, src) ∈ [(:cl, cl), (:ohlc, ohlc)]
+            @testset "$fname::$name" begin
+                let ta = f(src)
+                    @test ta.meta == src.meta
+                    @test length(ta) == 1
+                    @test ta.values == f(src.values, 1)
+                end
+
+                let ta = f(src, 2, corrected=false)
+                    @test ta.meta == src.meta
+                    @test length(ta) == length(src.timestamp)
+                    @test ta.values == f(src.values, 2, corrected=false)
+                    @test ta.colnames == [string(fname)]
+                end
+
+                @test_throws DimensionMismatch f(src, 3)
+            end  # @testset
+        end
+    end  # for func
 end
 
 
