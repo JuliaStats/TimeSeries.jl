09e0726d673586c249cfb31d33a7d9bbcb2dc225
Iblis Lin <iblis@hs.ntnu.edu.tw>
2018-10-11 19:49:49 +0800

    fix indentation in test/basemisc.jl

--- test/basemisc.jl
+++ test/basemisc.jl
@@ -103,136 +103,136 @@ end
 
 
 @testset "Base.any function" begin
-  let  # single column
-    """
-    julia> any(x .== lag(x))
-    1x1 TimeSeries.TimeArray{Bool,1,Date,BitArray{1}} 2000-01-07 to 2000-01-07
-    │            │ _     │
-    ├────────────┼───────┤
-    │ 2000-01-07 │ false │
-    """
-    let
-      ts = timestamp(cl)[1:5]
-      ta = TimeArray(ts, 1:5)
-      xs = any(ta .== lag(ta))
-      @test timestamp(xs)[1] == ts[end]
-      @test values(xs)[1]    == false
-    end
+    let  # single column
+        """
+        julia> any(x .== lag(x))
+        1x1 TimeSeries.TimeArray{Bool,1,Date,BitArray{1}} 2000-01-07 to 2000-01-07
+        │            │ _     │
+        ├────────────┼───────┤
+        │ 2000-01-07 │ false │
+        """
+        let
+            ts = timestamp(cl)[1:5]
+            ta = TimeArray(ts, 1:5)
+            xs = any(ta .== lag(ta))
+            @test timestamp(xs)[1] == ts[end]
+            @test values(xs)[1]    == false
+        end
 
-    """
-    julia> any(x .== lag(x), 2)
-    4x1 TimeSeries.TimeArray{Bool,1,Date,BitArray{1}} 2000-01-04 to 2000-01-07
-    │            │ any   │
-    ├────────────┼───────┤
-    │ 2000-01-04 │ false │
-    │ 2000-01-05 │ true  │
-    │ 2000-01-06 │ false │
-    │ 2000-01-07 │ false │
-    """
-    let
-      ts = timestamp(cl)[1:5]
-      ta = TimeArray(ts, [1, 2, 2, 3, 4])
-      xs = any(ta .== lag(ta), dims = 2)
+        """
+        julia> any(x .== lag(x), 2)
+        4x1 TimeSeries.TimeArray{Bool,1,Date,BitArray{1}} 2000-01-04 to 2000-01-07
+        │            │ any   │
+        ├────────────┼───────┤
+        │ 2000-01-04 │ false │
+        │ 2000-01-05 │ true  │
+        │ 2000-01-06 │ false │
+        │ 2000-01-07 │ false │
+        """
+        let
+            ts = timestamp(cl)[1:5]
+            ta = TimeArray(ts, [1, 2, 2, 3, 4])
+            xs = any(ta .== lag(ta), dims = 2)
 
-      @test timestamp(xs)   == ts[2:end]
-      @test any(values(xs) .== [false, true, false, false])
-      @test colnames(xs)    == [:any]
-    end
-  end  # single column
+            @test timestamp(xs)   == ts[2:end]
+            @test any(values(xs) .== [false, true, false, false])
+            @test colnames(xs)    == [:any]
+        end
+    end  # single column
 
-  let  # multi column
-    """
-    julia> x .> 3
-    3x2 TimeSeries.TimeArray{Bool,2,Date,BitArray{2}} 2000-01-03 to 2000-01-05
-    │            │ A     │ B     │
-    ├────────────┼───────┼───────┤
-    │ 2000-01-03 │ false │ false │
-    │ 2000-01-04 │ false │ true  │
-    │ 2000-01-05 │ true  │ true  │
+    let  # multi column
+        """
+        julia> x .> 3
+        3x2 TimeSeries.TimeArray{Bool,2,Date,BitArray{2}} 2000-01-03 to 2000-01-05
+        │            │ A     │ B     │
+        ├────────────┼───────┼───────┤
+        │ 2000-01-03 │ false │ false │
+        │ 2000-01-04 │ false │ true  │
+        │ 2000-01-05 │ true  │ true  │
 
-    julia> any(x .> 3, 2)
-    3x1 TimeSeries.TimeArray{Bool,2,Date,BitArray{2}} 2000-01-03 to 2000-01-05
-    │            │ any   │
-    ├────────────┼───────┤
-    │ 2000-01-03 │ false │
-    │ 2000-01-04 │ true  │
-    │ 2000-01-05 │ true  │
-    """
-    ts = timestamp(cl)[1:3]
-    ta = TimeArray(ts, [1 2; 3 4; 5 6])
-    xs = any(ta .> 3, dims = 2)
+        julia> any(x .> 3, 2)
+        3x1 TimeSeries.TimeArray{Bool,2,Date,BitArray{2}} 2000-01-03 to 2000-01-05
+        │            │ any   │
+        ├────────────┼───────┤
+        │ 2000-01-03 │ false │
+        │ 2000-01-04 │ true  │
+        │ 2000-01-05 │ true  │
+        """
+        ts = timestamp(cl)[1:3]
+        ta = TimeArray(ts, [1 2; 3 4; 5 6])
+        xs = any(ta .> 3, dims = 2)
 
-    @test timestamp(xs) == ts
-    @test any(values(xs) .== [false, true, true])
-    @test colnames(xs) == [:any]
-  end  # multi column
+        @test timestamp(xs) == ts
+        @test any(values(xs) .== [false, true, true])
+        @test colnames(xs) == [:any]
+    end  # multi column
 end  # @testset "Base.any" function
 
 
 @testset "Base.all function" begin
-  let  # single column
-    """
-    julia> all(x .== lag(x))
-    1x1 TimeSeries.TimeArray{Bool,1,Date,BitArray{1}} 2000-01-07 to 2000-01-07
-    │            │ A     │
-    ├────────────┼───────┤
-    │ 2000-01-07 │ false │
-    """
-    let
-      ts = timestamp(cl)[1:5]
-      ta = TimeArray(ts, 1:5)
-      xs = all(ta .== lag(ta))
-      @test timestamp(xs)[] == ts[end]
-      @test values(xs)[]    == false
-    end
+    let  # single column
+        """
+        julia> all(x .== lag(x))
+        1x1 TimeSeries.TimeArray{Bool,1,Date,BitArray{1}} 2000-01-07 to 2000-01-07
+        │            │ A     │
+        ├────────────┼───────┤
+        │ 2000-01-07 │ false │
+        """
+        let
+            ts = timestamp(cl)[1:5]
+            ta = TimeArray(ts, 1:5)
+            xs = all(ta .== lag(ta))
+            @test timestamp(xs)[] == ts[end]
+            @test values(xs)[]    == false
+        end
 
-    """
-    julia> all(x .== lag(x), 2)
-    4x1 TimeSeries.TimeArray{Bool,1,Date,BitArray{1}} 2000-01-04 to 2000-01-07
-    │            │ any   │
-    ├────────────┼───────┤
-    │ 2000-01-04 │ false │
-    │ 2000-01-05 │ true  │
-    │ 2000-01-06 │ false │
-    │ 2000-01-07 │ false │
-    """
-    let
-      ts = timestamp(cl)[1:5]
-      ta = TimeArray(ts, [1, 2, 2, 3, 4])
-      xs = all(ta .== lag(ta), dims = 2)
+        """
+        julia> all(x .== lag(x), 2)
+        4x1 TimeSeries.TimeArray{Bool,1,Date,BitArray{1}} 2000-01-04 to 2000-01-07
+        │            │ any   │
+        ├────────────┼───────┤
+        │ 2000-01-04 │ false │
+        │ 2000-01-05 │ true  │
+        │ 2000-01-06 │ false │
+        │ 2000-01-07 │ false │
+        """
+        let
+            ts = timestamp(cl)[1:5]
+            ta = TimeArray(ts, [1, 2, 2, 3, 4])
+            xs = all(ta .== lag(ta), dims = 2)
 
-      @test timestamp(xs) == ts[2:end]
-      @test all(values(xs) .== [false, true, false, false])
-      @test colnames(xs) == [:all]
-    end
-  end  # single column
+            @test timestamp(xs) == ts[2:end]
+            @test all(values(xs) .== [false, true, false, false])
+            @test colnames(xs) == [:all]
+        end
+    end  # single column
 
-  let  # multi column
-    """
-    julia> x .> 3
-    3x2 TimeSeries.TimeArray{Bool,2,Date,BitArray{2}} 2000-01-03 to 2000-01-05
-    │            │ A     │ B     │
-    ├────────────┼───────┼───────┤
-    │ 2000-01-03 │ false │ false │
-    │ 2000-01-04 │ false │ true  │
-    │ 2000-01-05 │ true  │ true  │
+    let  # multi column
+        """
+        julia> x .> 3
+        3x2 TimeSeries.TimeArray{Bool,2,Date,BitArray{2}} 2000-01-03 to 2000-01-05
+        │            │ A     │ B     │
+        ├────────────┼───────┼───────┤
+        │ 2000-01-03 │ false │ false │
+        │ 2000-01-04 │ false │ true  │
+        │ 2000-01-05 │ true  │ true  │
 
-    julia> all(x .> 3, 2)
-    3x1 TimeSeries.TimeArray{Bool,2,Date,BitArray{2}} 2000-01-03 to 2000-01-05
-    │            │ all   │
-    ├────────────┼───────┤
-    │ 2000-01-03 │ false │
-    │ 2000-01-04 │ false │
-    │ 2000-01-05 │ true  │
-    """
-    ts = timestamp(cl)[1:3]
-    ta = TimeArray(ts, [1 2; 3 4; 5 6])
-    xs = all(ta .> 3, dims = 2)
+        julia> all(x .> 3, 2)
+        3x1 TimeSeries.TimeArray{Bool,2,Date,BitArray{2}} 2000-01-03 to 2000-01-05
+        │            │ all   │
+        ├────────────┼───────┤
+        │ 2000-01-03 │ false │
+        │ 2000-01-04 │ false │
+        │ 2000-01-05 │ true  │
+        """
+        ts = timestamp(cl)[1:3]
+        ta = TimeArray(ts, [1 2; 3 4; 5 6])
+        xs = all(ta .> 3, dims = 2)
 
-    @test timestamp(xs) == ts
-    @test all(values(xs) .== [false, false, true])
-    @test colnames(xs) == [:all]
-  end  # multi column
+        @test timestamp(xs) == ts
+        @test all(values(xs) .== [false, false, true])
+        @test colnames(xs) == [:all]
+    end  # multi column
 end  # @testset "Base.all" function
 
 
