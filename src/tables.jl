7f10c3a90e6a7b493d234c98eefa05eadce149d1
Iblis Lin <iblis@hs.ntnu.edu.tw>
2018-10-28 17:51:54 +0800

    support Tables.rows

--- src/tables.jl
+++ src/tables.jl
@@ -1,27 +1,31 @@
 # JuliaData/Tables.jl integration
 
-const TableColumnIter = Tables.EachColumn{<:TimeArray}
+const TimeArrayColIter = Tables.EachColumn{<:TimeArray}
 
-Base.getindex(x::TableColumnIter{<:TimeArray}, i::Integer) =
-    (i == 1) ? timestamp(x.source) : x.source[colnames(x.source)[i - 1]]
-Base.length(x::TableColumnIter{<:TimeArray}) = length(colnames(x.source)) + 1
-Base.lastindex(x::TableColumnIter{<:TimeArray}) = length(colnames(x.source)) + 1
-Base.propertynames(x::TableColumnIter) = [:timestamp; propertynames(x.source)]
+source(x::TimeArrayColIter) = getfield(x, :source)
 
-function Base.iterate(x::TableColumnIter{<:TimeArray}, i = 1)
+Base.getindex(x::TimeArrayColIter{<:TimeArray}, i::Integer) =
+    (i == 1) ? timestamp(source(x)) : values(source(x)[colnames(source(x))[i - 1]])
+Base.length(x::TimeArrayColIter{<:TimeArray}) = length(colnames(source(x))) + 1
+Base.lastindex(x::TimeArrayColIter{<:TimeArray}) = length(colnames(source(x))) + 1
+Base.propertynames(x::TimeArrayColIter) = [:timestamp; propertynames(source(x))]
+Base.getproperty(x::TimeArrayColIter, c::Symbol) =
+    c == :timestamp ? timestamp(source(x)) : values(getproperty(source(x), c))
+
+function Base.iterate(x::TimeArrayColIter, i = 1)
     i > length(x) && return nothing
-    values(x[i]), i + 1
+    x[i], i + 1
 end
 
 Tables.istable(::Type{<:AbstractTimeSeries}) = true
-Tables.rowaccess(::Type{<:TimeArray}) = false
+Tables.rowaccess(::Type{<:TimeArray}) = true
 Tables.columnaccess(::Type{<:TimeArray}) = true
 Tables.columns(ta::TimeArray) = Tables.eachcolumn(ta)
-Tables.eachcolumn(i::TableColumnIter) = i
-
+Tables.eachcolumn(i::TimeArrayColIter) = i
+Tables.rows(ta::TimeArray) = Tables.rows(Tables.columntable(ta))
 Tables.schema(ta::TimeArray{T,N,D}) where {T,N,D} =
-    Table.Schema([:timestamp; colnames(ta)], (D, ntuple(x -> T, ndims(ta))...) )
-
+    Tables.Schema([:timestamp; colnames(ta)], (D, ntuple(x -> T, size(ta, 2))...) )
+Tables.schema(i::TimeArrayColIter) = Tables.schema(source(i))
 
 function TimeArray(x; on)
     Tables.istable(x) || throw(ArgumentError("TimeArray requires a table input"))
