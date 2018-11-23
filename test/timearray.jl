0bcb9e684ed00d0375470fedb26ed72e137147fc
scls19fr <scls19fr@users.noreply.github.com>
2018-11-23 11:26:29 +0100

    Update timearray.jl

--- test/timearray.jl
+++ test/timearray.jl
@@ -188,8 +188,8 @@ end
 end
 
 @testset "construct with NamedTuple" begin
-    data = (time=[DateTime(2018, 11, 21, 12, 0), DateTime(2018, 11, 21, 13, 0)], col1=[10.2, 11.2], col2=[20.2, 21.2], col3=[30.2, 31.2])
-    ta = TimeArray(data; meta="Example")
+    data = (datetime=[DateTime(2018, 11, 21, 12, 0), DateTime(2018, 11, 21, 13, 0)], col1=[10.2, 11.2], col2=[20.2, 21.2], col3=[30.2, 31.2])
+    ta = TimeArray(data; timestamp=:datetime, meta="Example")
     @test size(ta) == (2, 3)
     @test colnames(ta) == [:col1, :col2, :col3]
     @test timestamp(ta) == [DateTime(2018, 11, 21, 12, 0), DateTime(2018, 11, 21, 13, 0)]
