0a6738c3a648ef8c4b03cb440b0a34d14cdd8a4a
scls19fr <scls19fr@users.noreply.github.com>
2018-11-23 08:26:49 +0100

    Update timearray.jl

--- src/timearray.jl
+++ src/timearray.jl
@@ -80,6 +80,12 @@ TimeArray(ta::TimeArray;
           colnames = _colnames(ta), meta = _meta(ta), args...) =
     TimeArray(timestamp, values, colnames, meta; args...)
 
+function TimeArray(data::NamedTuple; timestamp=:time)
+    columns = (key for key in keys(data) if key != timestamp)
+    dat = hcat((data[key] for key in columns)...)
+    TimeArray(data[timestamp], dat, collect(columns))
+end
+
 ###### conversion ###############
 
 convert(::Type{TimeArray{Float64,N}}, x::TimeArray{Bool,N}) where N =
