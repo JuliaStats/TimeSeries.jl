39dc924cadfae99fcbbff4eb0654532f894ae7ae
Iblis Lin <iblis@hs.ntnu.edu.tw>
2019-06-15 19:38:29 +0800

    test cases

--- src/apply.jl
+++ src/apply.jl
@@ -106,7 +106,7 @@ function moving(f, ta::TimeArray{T,2}, window::Integer;
             vals[i, j] = f(@view(A[i:i+(window-1), j]))
         end
     else # case of dims = 2
-        vals = mapreduce(i -> f(view(A, i, :)), vcat, window:size(A, 1))
+        vals = mapreduce(i -> f(view(A, i-window+1:i, :)), vcat, window:size(A, 1))
         if size(vals, 2) != length(colnames)
             throw(ArgumentError(
                 "the output dims should match the lenght of columns, " *
--- test/apply.jl
+++ test/apply.jl
@@ -194,6 +194,20 @@ using TimeSeries
                 x[1]
             end
         end
+
+        @testset "moving with multi-column" begin
+            ta = moving(mean, ohlc, 1, dims = 2, colnames = [:mean])
+            ans = mean(ohlc, dims = 2)
+            @test all(values(ta) .== values(ans))
+            @test timestamp(ta)   == timestamp(ta)
+
+            ta = moving(ohlc, 10, dims = 2) do A
+                mean(A, dims = 1)
+            end
+            ans = moving(mean, ohlc, 10)
+            @test values(ta)       ≈ values(ans)
+            @test timestamp(ta)   == timestamp(ta)
+        end
     end
 
     @testset "upto method accumulates" begin
