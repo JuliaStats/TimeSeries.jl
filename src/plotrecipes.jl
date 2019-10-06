ee109af40e02cbf8d4a2d38d519c5ff2305d2c57
ohmsweetohm1 <50108075+ohmsweetohm1@users.noreply.github.com>
2019-10-06 16:34:12 +0200

    Fixed labels bug in plotrecipes.jl (#431)
    Fixes #430

--- src/plotrecipes.jl
+++ src/plotrecipes.jl
@@ -7,7 +7,7 @@
     #    ta, ohlc = extract_ohlc(ta)
     #    collect(zip(ohlc)) # But there are currently issues with that
     else
-        labels --> reshape(String.(colnames(ta)),1,length(colnames(ta)))
+        labels --> get(plotattributes, :label, reshape(String.(colnames(ta)),1,length(colnames(ta))))
         seriestype := st
         timestamp(ta), values(ta)
     end
